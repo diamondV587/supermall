ea610eb9661681a3feaa85b9ff47c8410410538b
leifu <570834681@qq.com>
2020-03-19 23:12:55 +0800

    network

--- src/network/request.js
+++ src/network/request.js
@@ -1,8 +1,6 @@
 import axios from 'axios'
 export function request(config){
   const instance = axios.create({
-    // http://106.54.54.237:8000/api/wh
-    baseURL:"http://123.207.32.32:8000/api/wh",
     timeout:5000
   })
 
